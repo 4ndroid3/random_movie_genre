0c68fa2e4a4611403789cf08f089d29594177836
4ndroid3 <andijaimo@gmail.com>
2023-02-15 20:27:40 -0300

    refactor, se sacan cosas sin uso

--- core/main.go
+++ core/main.go
@@ -5,20 +5,20 @@ import (
 
 	"github.com/4ndroid3/random_movie_genre/guardado"
 	"github.com/4ndroid3/random_movie_genre/lectura"
-	"github.com/4ndroid3/random_movie_genre/objeto"
 	"github.com/4ndroid3/random_movie_genre/random"
 )
 
 func main() {
 
+	fmt.Println()
 	fmt.Println("==--==--==--==--==--==--==--==--==")
 	fmt.Println("==--==--==--==--==--==--==--==--==")
 	fmt.Println("Inicia Randomizador de Peliculas")
 	fmt.Println("==--==--==--==--==--==--==--==--==")
-	fmt.Println("==--==--==--==--==--==--==--==--==")
+	fmt.Println("==--==--==--==--==--==--==--==--== ")
+	fmt.Println()
 
 	topicos := guardado.IngresoDeDatos(lectura.LeerTopicosEnArchivo())
-	objeto.GenerarTopicos(topicos)
 	guardado.GuardarTopicos(topicos)
 	random.GenerarObjetoRandom(topicos)
 }
--- core/objeto/objeto.go
+++ core/objeto/objeto.go
@@ -2,12 +2,6 @@ package objeto
 
 import "fmt"
 
-func GenerarTopicos(ingresado []string) []string {
-	fmt.Printf("Dato ingresado: %v \n", ingresado)
-
-	return ingresado
-}
-
 func AgregaTopico(topico string) string {
 	fmt.Printf("Dato ingresado: %v \n \n", topico)
 	return topico
